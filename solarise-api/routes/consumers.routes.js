d093cabd3f76be9cd19da18dd04dd8f8651c7b85
Shibu-tech <shibuprasadnayak893@gmail.com>
2026-07-25 17:19:38 +0530

    Add files via upload

--- solarise-api/routes/consumers.routes.js
+++ solarise-api/routes/consumers.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getAllConsumers } from "../controllers/consumers.controller.js";
+import { getAllConsumers,getConsumerById,createConsumer,updateConsumer,deleteConsumer } from "../controllers/consumers.controller.js";
 
 const router = Router();
 
 router.get("/", getAllConsumers);
+router.get("/:id", getConsumerById);
+router.post("/", createConsumer);
+router.put("/:id", updateConsumer);
+router.delete("/:id", deleteConsumer);
 
 export default router;
