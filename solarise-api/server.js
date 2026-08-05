b1e091b19643e57260d772b7e852ec6382597e77
Shibu-tech <shibuprasadnayak893@gmail.com>
2026-08-05 17:26:32 +0530

    Authentication done

--- solarise-api/server.js
+++ solarise-api/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import pool from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 import areaBlocksRoutes from "./routes/areaBlocks.routes.js";
 import consumersRoutes from "./routes/consumers.routes.js";
@@ -14,17 +15,18 @@ import ownershipTransfersRoutes from "./routes/ownershipTransfers.routes.js";
 import materialDeliveriesRoutes from "./routes/materialDeliveries.routes.js";
 import installationProgressRoutes from "./routes/installationProgress.routes.js";
 import paymentsRoutes from "./routes/payments.routes.js";
-import notificationRoutes from "./routes/notifications.routes.js";
-import statusHistoryRoutes from "./routes/statusHistory.routes.js";
+import { authenticateToken } from "./middleware/auth.middleware.js";
 const app = express();
 const PORT = process.env.PORT || 'your_port';
 
 app.use(cors());
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.use("/api/auth", authRoutes);
+app.use(authenticateToken);
 app.use("/api/users", usersRoutes);
 app.use("/api/areaBlocks", areaBlocksRoutes);
 app.use('/api/consumers', consumersRoutes);
@@ -36,8 +38,6 @@ app.use("/api/ownership-transfers", ownershipTransfersRoutes);
 app.use("/api/material-deliveries", materialDeliveriesRoutes);
 app.use("/api/installation", installationProgressRoutes);
 app.use("/api/payments", paymentsRoutes);
-app.use('/api/notifications', notificationRoutes);
-app.use('/api/status-history', statusHistoryRoutes);
 
 pool.query("SELECT NOW()", (err, res) => {
     if (err) {
