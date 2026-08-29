ae03123be69496d0f3b6d70384fb1e158913df2f
SHIBU PRASAD NAYAK <shibuprasadnayak893@gmail.com>
2026-08-29 12:24:27 +0530

    Neon db changes

--- solarise-api/config/db.js
+++ solarise-api/config/db.js
@@ -2,16 +2,13 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 const pool = new pg.Pool({
-    user: process.env.DB_USER || "username",
-    host: process.env.DB_HOST || "host",
-    database: process.env.DB_NAME || "db_name",
-    password: process.env.DB_PASSWORD || "your_password",
-    port: process.env.DB_PORT || 'portno',
-    options: "-c search_path=solarise,public",
-    ssl: process.env.DB_SSL === 'true' || process.env.DB_HOST?.includes('neon.tech') ? { rejectUnauthorized: false } : false,
-    max: 20, // Maximum connections in pool
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 20,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionTimeoutMillis: 10000
 });
 
 pool.on('connect', () => {
